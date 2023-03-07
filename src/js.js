d27491f8e3417620b4d0c628d46e250ef82ff45f
Kindziolek <kingapietron@gmail.com>
2023-03-07 03:39:13 +0100

    List fix

--- src/js.js
+++ src/js.js
@@ -1,9 +1,23 @@
 const list = ["aaa", "bbb", "ccc", "ddd"];
 
+// list.forEach(el => {
+//   let div = document.createElement("div");
+//   document.querySelector('.box').append(div, el);
+//   div.classList.add("item");
+// });
+
+// Dodaje do pierwszeo tylko:
+// list.forEach(el => {
+//   let div = document.createElement("div");
+//   document.querySelector('.box').append(div);
+//   div.classList.add("item");
+//   document.querySelector(".item").append(el)
+// });
+
+
 list.forEach(el => {
   let div = document.createElement("div");
-  document.querySelector('.box').append(div, el);
+  document.querySelector('.box').append(div);
   div.classList.add("item");
+  div.innerHTML = `${el}`;
 });
-
-
