b95595f7012e3325bc136bc65314d17e56bf3264
Kindziolek <kingapietron@gmail.com>
2023-03-10 00:49:43 +0100

    onClick

--- src/js.js
+++ src/js.js
@@ -1,18 +1,23 @@
-const list = ["aaa", "bbb", "ccc", "ddd", "eee"];
+const list = [];
 
-list.forEach((el) => {
-  let div = document.createElement("div");
-  document.querySelector(".list").append(div);
-  div.classList.add("item");
-  div.innerHTML = `${el}`;
-});
+function renderList() {
+  document.querySelector(".list").innerHTML = ""
+  list.forEach((el) => {
+    let div = document.createElement("div");
+    document.querySelector(".list").append(div);
+    div.classList.add("item");
+    div.innerHTML = `${el}`;
+  });
+}
+
+renderList();
 
 const addItem = document.querySelector(".add_btn");
 
-const todo = document.querySelector(".todo");
 
 addItem.addEventListener("click", () => {
+  const todo = document.querySelector('input').value;
   list.push(`${todo}`);
-  console.log(list);
-  alert("eh");
+
+  renderList();
 });
