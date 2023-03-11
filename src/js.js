4e4c515b4d7f0e728ffe9e3f57491dbfe0ce34dc
Kindziolek <kingapietron@gmail.com>
2023-03-11 02:52:28 +0100

    remove span class

--- src/js.js
+++ src/js.js
@@ -1,23 +1,38 @@
 const list = [];
 
 function renderList() {
-  document.querySelector(".list").innerHTML = ""
+  document.querySelector(".list").innerHTML = "";
+  document.querySelector("input").innerHTML = "";
+
   list.forEach((el) => {
     let div = document.createElement("div");
+
     document.querySelector(".list").append(div);
     div.classList.add("item");
-    div.innerHTML = `${el}`;
+    div.innerHTML = (`${el} <span class="remove"></span>`);
+
+  
+  
+      let remove = document.querySelector(".remove")
+     
+      remove.addEventListener("click", () => {
+        alert("ss")
+        // remove.parentElement.removeChild(remove)
+        
+        renderList();
+        event.preventDefault();
+      });
+    
+
   });
 }
 
 renderList();
 
-const addItem = document.querySelector(".add_btn");
+const button = document.querySelector(".add_btn");
 
-
-
-addItem.addEventListener("click", () => {
-  const todo = document.querySelector('input').value;
+button.addEventListener("click", () => {
+  const todo = document.querySelector("input").value;
   list.push(`${todo}`);
 
   renderList();
