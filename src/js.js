a9ca7b7cbe9b343f4e7b361318cbb73e13e00a61
Kindziolek <kingapietron@gmail.com>
2023-03-28 20:05:05 +0200

    Add btn

--- src/js.js
+++ src/js.js
@@ -1,78 +1,25 @@
-const items = ["ss", "sss", "kkk"]
+const items = ["ss", "sss", "kkk"];
 
 let list = document.querySelector("#list");
-let itemAdd = document.querySelector("input");
 let addBtn = document.querySelector("#btn");
 
-let li = document.createElement("li");
+let removeBtn = document.querySelector(".remove");
 
 function renderList() {
-  for(let item of items) {
-    list.innerHTML+=`<li class="item"> ${item} </li>`
-  }
+  document.querySelector("#list").innerHTML = "";
+
+  items.forEach((el) => {
+    list.innerHTML += `<li class="item"> ${el} <span class="remove"></span> </li>`;
+  });
 }
-renderList()
 
+renderList();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const list = ["ss", "sssss"];
-
-// function renderList() {
-//   document.querySelector(".list").innerHTML = "";
-//   document.querySelector("input").innerHTML = "";
-
-//   list.forEach((el) => {
-//     let div = document.createElement("div");
-
-//     document.querySelector(".list").append(div);
-//     div.classList.add("item");
-//     div.innerHTML = `${el} <span class="remove"></span>`;
-
-//     let removeItem = div.querySelector(".remove");
-    
-//     removeItem.addEventListener("click", (event) => {
-//      list = list.filter(k => k !== el);
-      
-
-//       renderList();
-//       event.preventDefault();
-//     });
-//   });
-// }
-
-// renderList();
-
-// const button = document.querySelector(".add_btn");
-
-// button.addEventListener("click", () => {
-//   const todo = document.querySelector("input").value;
-//   list.push(`${todo}`);
-//   renderList();
-//   event.preventDefault();
-// });
-
-
-
-
-
+addBtn.addEventListener("click", () => {
+  const newItem = document.querySelector("input").value;
+  items.push(newItem);
+  renderList();
+  e.preventDefault();
+});
 
 
