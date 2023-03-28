77c1ed1b7d522c24aae5016d2b9ddd9f43a98c7d
Kindziolek <kingapietron@gmail.com>
2023-03-28 18:56:12 +0200

    TODO vol.2

--- src/js.js
+++ src/js.js
@@ -1,35 +1,78 @@
-const list = ["ss", "sssss"];
+const items = ["ss", "sss", "kkk"]
+
+let list = document.querySelector("#list");
+let itemAdd = document.querySelector("input");
+let addBtn = document.querySelector("#btn");
+
+let li = document.createElement("li");
 
 function renderList() {
-  document.querySelector(".list").innerHTML = "";
-  document.querySelector("input").innerHTML = "";
+  for(let item of items) {
+    list.innerHTML+=`<li class="item"> ${item} </li>`
+  }
+}
+renderList()
 
-  list.forEach((el) => {
-    let div = document.createElement("div");
 
-    document.querySelector(".list").append(div);
-    div.classList.add("item");
-    div.innerHTML = `${el} <span class="remove"></span>`;
 
-    let removeItem = div.querySelector(".remove");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const list = ["ss", "sssss"];
+
+// function renderList() {
+//   document.querySelector(".list").innerHTML = "";
+//   document.querySelector("input").innerHTML = "";
+
+//   list.forEach((el) => {
+//     let div = document.createElement("div");
+
+//     document.querySelector(".list").append(div);
+//     div.classList.add("item");
+//     div.innerHTML = `${el} <span class="remove"></span>`;
+
+//     let removeItem = div.querySelector(".remove");
     
-    removeItem.addEventListener("click", (event) => {
-     list = list.filter(k => k !== el);
+//     removeItem.addEventListener("click", (event) => {
+//      list = list.filter(k => k !== el);
       
 
-      renderList();
-      event.preventDefault();
-    });
-  });
-}
+//       renderList();
+//       event.preventDefault();
+//     });
+//   });
+// }
 
-renderList();
+// renderList();
 
-const button = document.querySelector(".add_btn");
+// const button = document.querySelector(".add_btn");
 
-button.addEventListener("click", () => {
-  const todo = document.querySelector("input").value;
-  list.push(`${todo}`);
-  renderList();
-  event.preventDefault();
-});
+// button.addEventListener("click", () => {
+//   const todo = document.querySelector("input").value;
+//   list.push(`${todo}`);
+//   renderList();
+//   event.preventDefault();
+// });
+
+
+
+
+
+
+
