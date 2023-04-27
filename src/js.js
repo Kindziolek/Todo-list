9a90acc765f11f1062133864fceafa63b104ca29
Kindziolek <kingapietron@gmail.com>
2023-04-27 02:45:57 +0200

    reset and css

--- src/js.js
+++ src/js.js
@@ -1,27 +1,5 @@
 let todoItems = [];
 
-function addTodo(text) {
-  const todo = {
-    text,
-    checked: false,
-    id: Date.now(),
-  };
-
-  todoItems.push(todo);
-}
-
-const form = document.querySelector("#form");
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const input = document.querySelector("#text");
-  const text = input.value.trim();
-  if (text !== "") {
-    addTodo(text);
-    input.value = "";
-    input.focus();
-  }
-});
-
 function renderTodo(todo) {
   const list = document.querySelector("#list");
   const isChecked = todo.checked ? "done" : "";
@@ -33,10 +11,37 @@ function renderTodo(todo) {
   node.innerHTML= `
   <input id="${todo.id}" type="checkbox"/>
   <label for="${todo.id}" class="tick js-tick"></label>
-  <span>${todo.text}</span>
-  <button class="delete-todo js-delete-todo">
-  <svg><use href="#delete-icon"></use></svg>
+  <span class="item_text">${todo.text}</span>
+  <button class="remove js-delete-todo">
+  <img src="./image/icon_remove.png" width="20px" height="20px">
   </button>`;
 
   list.append(node);
+  
 }
+
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
+
+  todoItems.push(todo);
+  renderTodo(todo)
+
+}
+
+const form = document.querySelector("#form");
+form.addEventListener("click", (event) => {
+  event.preventDefault();
+  const input = document.querySelector("#text");
+
+  const text = input.value.trim();
+  if (text !== '') {
+    addTodo(text);
+    input.value = '';
+    input.focus();
+  }
+});
+
